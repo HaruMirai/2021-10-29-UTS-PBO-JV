53b11db7a9f13165e6b4815c414a061cea34ef6d
HaruMirai <jasonvitonew@gmail.com>
2021-10-29 19:13:33 +0700

    update hari

--- konvwaktu.py
+++ konvwaktu.py
@@ -1,9 +1,11 @@
 detik = int(input("Masukkan detik= "))
 
-jam = detik / 3600
-sisa1 = detik % 3600
-menit = sisa1 / 60
+hari = detik / 86400
+sisa1 = detik % 86400
+jam = sisa1 / 3600
+sisa2 = detik % 3600
+menit = sisa2 / 60
 dtk = sisa1 % 60
 
-print(jam, "H", " ", menit, "m"," ", dtk, "s")
+print(hari, "D", " " ,jam, "H", " ", menit, "m"," ", dtk, "s")
 
